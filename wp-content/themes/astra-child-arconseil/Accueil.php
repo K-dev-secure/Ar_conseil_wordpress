6c62bee5b764c6ea471c4461836cbfe6d00a81fc
koffiwilly <willy.koffi@outlook.fr>
2026-04-01 01:17:29 +0200

    optimisation de la vidéo

--- wp-content/themes/astra-child-arconseil/Accueil.php
+++ wp-content/themes/astra-child-arconseil/Accueil.php
@@ -12,12 +12,15 @@ get_header(); ?>
 </style>
 
 <div class="background_header_accueil">
-    <video class="video_banniere_accueil" autoplay muted loop playsinline preload="metadata"
-        aria-hidden="true">
-        <source
-            src="https://arconseils-patrimoine.com/wp-content/uploads/2026/03/video-accueil.mp4"
-            type="video/mp4">
-    </video>
+    <video class="video_banniere_accueil" 
+       autoplay 
+       muted 
+       loop 
+       playsinline 
+       preload="auto" 
+       poster="https://arconseils-patrimoine.com/wp-content/uploads/2026/03/image-attente.jpg">
+    <source src="https://arconseils-patrimoine.com/wp-content/uploads/2026/03/video-accueil.mp4" type="video/mp4">
+</video>
     <h1><?php the_title(); ?></h1>
     <h2><?php the_field('sous_titre_header'); ?></h2>
 </div>
