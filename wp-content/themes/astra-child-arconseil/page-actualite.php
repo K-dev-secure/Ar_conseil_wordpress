05e12c61bef3ff4c5375709f453750cbb5b96686
koffiwilly <willy.koffi@outlook.fr>
2026-04-01 02:15:27 +0200

    en construction

--- wp-content/themes/astra-child-arconseil/page-actualite.php
+++ wp-content/themes/astra-child-arconseil/page-actualite.php
@@ -5,6 +5,9 @@
 
 get_header(); ?>
 
-
+<div style="text-align:center; margin: 100px 0;">
+	<h1 style="font-size:3em; color:#c00;">EN CONSTRUCTION</h1>
+	<p>Cette page sera bientôt disponible.</p>
+</div>
 
 <?php  get_footer(); ?>
